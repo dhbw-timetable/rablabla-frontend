dc5be2c5aa84589391b1598eb488130540d092d9
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-06-22 12:47:55 +0200

    Change production route

--- src/BackendConnection.js
+++ src/BackendConnection.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import moment from 'moment';
 
 const publicDomain = 'rablabla.m320trololol.com';
-const productionBackend = 'https://wailord:9001';
+const productionBackend = 'https://backendrablabla.m320trololol.com/';
 const ajaxTarget = window.location.href.indexOf(publicDomain) !== -1 ? productionBackend : 'http://localhost:10010';
 /* const sampleBackendResponse = {
   '18.06.2018': [
